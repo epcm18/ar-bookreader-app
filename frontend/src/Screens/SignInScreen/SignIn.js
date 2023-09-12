f8def2e242acb40330f6c7cf7c94a3c78cbcf2d1
Menuka L.D.S <104838986+sanuthmenuka@users.noreply.github.com>
2023-09-12 17:03:39 +0530

    authentication complete

--- frontend/src/Screens/SignInScreen/SignIn.js
+++ frontend/src/Screens/SignInScreen/SignIn.js
@@ -10,15 +10,18 @@ import {
 import Logo from "../../../assets/Logo/png/logo-white.png";
 import CustomInput from "../../components/CustomInput/CustomInput";
 import CustomButton from "../../components/CustomButton/CustomButton";
+import { useNavigation } from "@react-navigation/native";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
+  const navigation = useNavigation();
 
   const onSignInPressed = async (e) => {
     e.preventDefault();
     await login(email, password);
+    navigation.navigate("HomeScreen");
   };
 
   const onForgotPasswordPressed = () => {
